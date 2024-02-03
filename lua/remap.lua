9d48847c6fe34c937ee6e870ed13fd7bd0118ab0
Balint Orosz <orosz.balint.97@gmail.com>
2024-02-03 16:28:07 +0100

    fix remaps

--- lua/remap.lua
+++ lua/remap.lua
@@ -5,5 +5,5 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<leader>ss", "<cmd>vsplit | wincmd L | Telescope git_files<cr>")
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>cprev<CR>zz")
