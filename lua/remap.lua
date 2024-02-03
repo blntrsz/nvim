f9e0fa24b4a6d55a7208a667d76a6a138551da78
Balint Orosz <orosz.balint.97@gmail.com>
2024-02-03 16:22:18 +0100

    add lsp config and quickfix remap

--- lua/remap.lua
+++ lua/remap.lua
@@ -4,3 +4,6 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<leader>ss", "<cmd>vsplit | wincmd L | Telescope git_files<cr>")
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
