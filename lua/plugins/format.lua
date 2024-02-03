db4a8467b4b9291c2edda22680bdc6b531e8276f
Balint Orosz <orosz.balint.97@gmail.com>
2024-02-03 21:39:02 +0100

    increase formatter timeout

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -14,7 +14,7 @@ return {
 				typescriptreact = { "prettier" },
 			},
 			-- Set up format-on-save
-			format_on_save = { timeout_ms = 500, lsp_fallback = true },
+			format_on_save = { timeout_ms = 1000, lsp_fallback = true },
 			-- Customize formatters
 		},
 		init = function()
