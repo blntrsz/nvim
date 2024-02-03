88693d12cc2341f72c719d2d363e296a1d7f9ff7
Balint Orosz <orosz.balint.97@gmail.com>
2024-02-03 18:35:03 +0100

    merge treesitter into init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,14 @@
 return {
 	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+
+				ensure_installed = { "javascript", "typescript", "tsx", "css", "html" },
+			})
+		end,
+	},
+	{
 		"nvim-lua/plenary.nvim",
 		name = "plenary",
 	},
--- lua/plugins/treesitter.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"nvim-treesitter/nvim-treesitter",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-
-			ensure_installed = { "javascript", "typescript", "tsx", "css", "html" },
-		})
-	end,
-}
