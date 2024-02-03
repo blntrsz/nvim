e29dc2487ad92331aefe1ae34b8c487ed55a74d0
Balint Orosz <orosz.balint.97@gmail.com>
2024-02-03 21:28:41 +0100

    add prettier for typescript and tsreact

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -21,6 +21,8 @@ return {
 				lua = { "stylua" },
 				python = { "isort", "black" },
 				javascript = { { "prettierd", "prettier" } },
+				typescript = { { "prettierd", "prettier" } },
+				typescriptreact = { { "prettierd", "prettier" } },
 			},
 			-- Set up format-on-save
 			format_on_save = { timeout_ms = 500, lsp_fallback = true },
