2ac2c8b45c2a24d2ab4dbf29392fc0479bfde23d
Orosz Bálint <balint@Orosz-MacBook-Pro.local>
2024-05-07 21:14:11 +0200

    move to vim illuminate

--- init.lua
+++ init.lua
@@ -3,26 +3,26 @@ require("remap")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
 vim.filetype.add({
-	extension = {
-		templ = "templ",
-	},
+  extension = {
+    templ = "templ",
+  },
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "qf" },
-	command = [[nnoremap <buffer> <CR> <CR>:cclose<CR>]],
+  pattern = { "qf" },
+  command = [[nnoremap <buffer> <CR> <CR>:cclose<CR>]],
 })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,152 +1,149 @@
 return {
-	{
-		"AckslD/nvim-neoclip.lua",
-		requires = {
-			{ "kkharji/sqlite.lua", module = "sqlite" },
-			{ "nvim-telescope/telescope.nvim" },
-		},
-		keys = {
-			{ "<leader>nn", "<cmd>Telescope neoclip<cr>" },
-		},
-		config = function()
-			require("neoclip").setup({
-				default_register = "+",
-			})
-		end,
-	},
-	{
-		"windwp/nvim-ts-autotag",
-		config = function()
-			require("nvim-treesitter.configs").setup({})
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
+  {
+    "AckslD/nvim-neoclip.lua",
+    requires = {
+      { "kkharji/sqlite.lua",           module = "sqlite" },
+      { "nvim-telescope/telescope.nvim" },
+    },
+    keys = {
+      { "<leader>nn", "<cmd>Telescope neoclip<cr>" },
+    },
+    config = function()
+      require("neoclip").setup({
+        default_register = "+",
+      })
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-treesitter.configs").setup({})
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
 
-				ensure_installed = { "javascript", "typescript", "tsx", "css", "html" },
-				auto_install = true,
+        ensure_installed = { "javascript", "typescript", "tsx", "css", "html" },
+        auto_install = true,
 
-				indent = {
-					enable = true,
-				},
+        indent = {
+          enable = true,
+        },
 
-				highlight = {
-					-- `false` will disable the whole extension
-					enable = true,
+        highlight = {
+          -- `false` will disable the whole extension
+          enable = true,
 
-					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-					-- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-					-- Using this option may slow down your editor, and you may see some duplicate highlights.
-					-- Instead of true it can also be a list of languages
-					additional_vim_regex_highlighting = { "markdown" },
-				},
-			})
-			local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-			treesitter_parser_config.templ = {
-				install_info = {
-					url = "https://github.com/vrischmann/tree-sitter-templ.git",
-					files = { "src/parser.c", "src/scanner.c" },
-					branch = "master",
-				},
-			}
+          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+          -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
+          -- Using this option may slow down your editor, and you may see some duplicate highlights.
+          -- Instead of true it can also be a list of languages
+          additional_vim_regex_highlighting = { "markdown" },
+        },
+      })
+      local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      treesitter_parser_config.templ = {
+        install_info = {
+          url = "https://github.com/vrischmann/tree-sitter-templ.git",
+          files = { "src/parser.c", "src/scanner.c" },
+          branch = "master",
+        },
+      }
 
-			vim.treesitter.language.register("templ", "templ")
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter-context",
-		config = function()
-			require("treesitter-context").setup({
-				enable = true,
-				max_lines = 0,
-				min_window_height = 0,
-				line_numbers = true,
-				multiline_threshold = 20,
-				trim_scope = "outer",
-				mode = "cursor",
-				separator = nil,
-				zindex = 20,
-				on_attach = nil,
-			})
-		end,
-	},
-	{
-		"nvim-lua/plenary.nvim",
-		name = "plenary",
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
-	{
-		"yamatsum/nvim-cursorline",
-		config = function()
-			require("nvim-cursorline").setup()
-		end,
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		event = "BufEnter",
-		config = function()
-			require("lualine").setup()
-		end,
-	},
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		opts = {}, -- this is equalent to setup({}) function
-	},
-	-- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
-	{
-		"numToStr/Comment.nvim",
-		opts = {
-			-- add any options here
-		},
-		lazy = false,
-	},
-	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
-	{
-		"xiyaowong/transparent.nvim",
-		name = "transparent",
-		lazy = false,
-		config = function()
-			require("transparent").setup({
-				extra_groups = {
-					"NormalFloat",
-				},
-			})
-			require("transparent").clear_prefix("nvim-treesitter/nvim-treesitter-context")
-		end,
-	},
-	{
-		"folke/neodev.nvim",
-		config = function()
-			require("neodev").setup()
-		end,
-	},
-	{
-		"mbbill/undotree",
-		keys = {
-			{ "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Undotree" },
-		},
-	},
-	{
-		"nvim-pack/nvim-spectre",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		keys = {
-			{ "<leader>F", '<cmd>lua require("spectre").toggle()<cr>' },
-		},
-	},
-	{
-		"kamykn/spelunker.vim",
-		config = function()
-			vim.cmd("highlight SpelunkerSpellBad cterm=undercurl gui=undercurl guisp=NONE guifg=NONE")
-			vim.cmd(
-				"highlight SpelunkerComplexOrCompoundWord cterm=undercurl ctermfg=NONE gui=undercurl guifg=NONE guisp=NONE"
-			)
-		end,
-	},
+      vim.treesitter.language.register("templ", "templ")
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    config = function()
+      require("treesitter-context").setup({
+        enable = true,
+        max_lines = 0,
+        min_window_height = 0,
+        line_numbers = true,
+        multiline_threshold = 20,
+        trim_scope = "outer",
+        mode = "cursor",
+        separator = nil,
+        zindex = 20,
+        on_attach = nil,
+      })
+    end,
+  },
+  {
+    "nvim-lua/plenary.nvim",
+    name = "plenary",
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  {
+    "RRethy/vim-illuminate",
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "BufEnter",
+    config = function()
+      require("lualine").setup()
+    end,
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {}, -- this is equalent to setup({}) function
+  },
+  -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+  {
+    "numToStr/Comment.nvim",
+    opts = {
+      -- add any options here
+    },
+    lazy = false,
+  },
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+  {
+    "xiyaowong/transparent.nvim",
+    name = "transparent",
+    lazy = false,
+    config = function()
+      require("transparent").setup({
+        extra_groups = {
+          "NormalFloat",
+        },
+      })
+      require("transparent").clear_prefix("nvim-treesitter/nvim-treesitter-context")
+    end,
+  },
+  {
+    "folke/neodev.nvim",
+    config = function()
+      require("neodev").setup()
+    end,
+  },
+  {
+    "mbbill/undotree",
+    keys = {
+      { "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Undotree" },
+    },
+  },
+  {
+    "nvim-pack/nvim-spectre",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>F", '<cmd>lua require("spectre").toggle()<cr>' },
+    },
+  },
+  {
+    "kamykn/spelunker.vim",
+    config = function()
+      vim.cmd("highlight SpelunkerSpellBad cterm=undercurl gui=undercurl guisp=NONE guifg=NONE")
+      vim.cmd(
+        "highlight SpelunkerComplexOrCompoundWord cterm=undercurl ctermfg=NONE gui=undercurl guifg=NONE guisp=NONE"
+      )
+    end,
+  },
 }
