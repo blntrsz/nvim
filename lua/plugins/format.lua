826f42f6678f81667260859ef5f82fae18ade5d1
Orosz Bálint <balint@Orosz-MacBook-Pro.local>
2024-05-16 20:51:51 +0200

    add snippets

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,27 +1,27 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
-		-- Everything in opts will be passed to setup()
-		opts = {
-			-- Define your formatters
-			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "isort", "black" },
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				typescriptreact = { "prettier" },
-				go = { "goimports", "gofmt" },
-				templ = { "goimports", "templ" },
-			},
-			-- Set up format-on-save
-			format_on_save = { timeout_ms = 1000, lsp_fallback = true },
-			-- Customize formatters
-		},
-		init = function()
-			-- If you want the formatexpr, here is the place to set it
-			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-		end,
-	},
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    -- Everything in opts will be passed to setup()
+    opts = {
+      -- Define your formatters
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        go = { "goimports", "gofmt" },
+        templ = { "goimports", "templ" },
+      },
+      -- Set up format-on-save
+      format_on_save = { timeout_ms = 1000, lsp_fallback = true },
+      -- Customize formatters
+    },
+    init = function()
+      -- If you want the formatexpr, here is the place to set it
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+  },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,138 +1,164 @@
 return {
-	{
-		"nvimdev/lspsaga.nvim",
-		after = "nvim-lspconfig",
-		keys = {
-			{ "<leader>ca", "<cmd>Lspsaga code_action<cr>" },
-			{ "<leader>cr", "<cmd>Lspsaga rename<cr>" },
-			{ "K", "<cmd>Lspsaga hover_doc<cr>" },
-			{ "<leader>o", "<cmd>Lspsaga outline<cr>" },
-		},
-		config = function()
-			require("lspsaga").setup({})
-		end,
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-tree/nvim-web-devicons",
-		},
-	},
-	{
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+  },
+  {
+    "nvimdev/lspsaga.nvim",
+    after = "nvim-lspconfig",
+    keys = {
+      { "<leader>ca", "<cmd>Lspsaga code_action<cr>" },
+      { "<leader>cr", "<cmd>Lspsaga rename<cr>" },
+      { "K",          "<cmd>Lspsaga hover_doc<cr>" },
+      { "<leader>o",  "<cmd>Lspsaga outline<cr>" },
+    },
+    config = function()
+      require("lspsaga").setup({})
+    end,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
+  {
 
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
-			"hrsh7th/nvim-cmp",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-			"j-hui/fidget.nvim",
-		},
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/nvim-cmp",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "j-hui/fidget.nvim",
+    },
 
-		config = function()
-			local cmp = require("cmp")
-			local cmp_lsp = require("cmp_nvim_lsp")
-			local capabilities = vim.tbl_deep_extend(
-				"force",
-				{},
-				vim.lsp.protocol.make_client_capabilities(),
-				cmp_lsp.default_capabilities()
-			)
+    config = function()
+      local cmp = require("cmp")
+      local cmp_lsp = require("cmp_nvim_lsp")
+      local capabilities = vim.tbl_deep_extend(
+        "force",
+        {},
+        vim.lsp.protocol.make_client_capabilities(),
+        cmp_lsp.default_capabilities()
+      )
 
-			require("fidget").setup({})
-			require("mason").setup()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"tsserver",
-				},
-				handlers = {
-					function(server_name) -- default handler (optional)
-						require("lspconfig")[server_name].setup({
-							capabilities = capabilities,
-						})
-					end,
+      require("fidget").setup({})
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",
+        },
+        handlers = {
+          function(server_name) -- default handler (optional)
+            require("lspconfig")[server_name].setup({
+              capabilities = capabilities,
+            })
+          end,
 
-					["lua_ls"] = function()
-						local lspconfig = require("lspconfig")
-						lspconfig.lua_ls.setup({
-							capabilities = capabilities,
-							settings = {
-								Lua = {
-									diagnostics = {
-										globals = { "vim", "it", "describe", "before_each", "after_each" },
-									},
-								},
-							},
-						})
-					end,
-				},
-			})
+          ["lua_ls"] = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({
+              capabilities = capabilities,
+              settings = {
+                Lua = {
+                  diagnostics = {
+                    globals = { "vim", "it", "describe", "before_each", "after_each" },
+                  },
+                },
+              },
+            })
+          end,
+        },
+      })
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-p>"] = cmp.mapping.select_prev_item(),
-					["<C-n>"] = cmp.mapping.select_next_item(),
-					["<C-y>"] = cmp.mapping.confirm({ select = true }),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-u>"] = cmp.mapping.scroll_docs(-4),
-					["<C-d>"] = cmp.mapping.scroll_docs(4),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- For luasnip users.
-					{
-						name = "spell",
-						option = {
-							keep_all_entries = false,
-							enable_in_context = function()
-								return true
-							end,
-						},
-					},
-				}, {
-					{ name = "buffer" },
-				}),
-			})
+      local ls = require "luasnip"
+      ls.config.set_config {
+        history = false,
+        updateevents = "TextChanged,TextChangedI",
+      }
 
-			vim.diagnostic.config({
-				-- update_in_insert = true,
-				float = {
-					focusable = false,
-					style = "minimal",
-					border = "rounded",
-					source = "always",
-					header = "",
-					prefix = "",
-				},
-			})
-			vim.keymap.set("n", "<leader>ws", function()
-				vim.lsp.buf.workspace_symbol()
-			end)
-			vim.keymap.set("n", "gr", function()
-				require("telescope.builtin").lsp_references()
-			end)
-			vim.keymap.set("n", "<leader>q", function()
-				vim.diagnostic.open_float()
-			end)
-			vim.keymap.set("n", "gd", function()
-				vim.lsp.buf.definition()
-			end)
-			vim.keymap.set("n", "<leader>l", function()
-				vim.diagnostic.goto_next()
-			end)
-			vim.keymap.set("n", "<leader>h", function()
-				vim.diagnostic.goto_prev()
-			end)
-		end,
-	},
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      vim.keymap.set({ "i", "s" }, "<c-l>", function()
+        if ls.expand_or_jumpable() then
+          ls.expand_or_jump()
+        end
+      end, { silent = true })
+
+      vim.keymap.set({ "i", "s" }, "<c-h>", function()
+        if ls.jumpable(-1) then
+          ls.jump(-1)
+        end
+      end, { silent = true })
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-d>"] = cmp.mapping.scroll_docs(4),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, -- For luasnip users.
+          { name = "path" },
+          { name = "buffer" },
+          {
+            name = "spell",
+            option = {
+              keep_all_entries = false,
+              enable_in_context = function()
+                return true
+              end,
+            },
+          },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+
+      vim.diagnostic.config({
+        -- update_in_insert = true,
+        float = {
+          focusable = false,
+          style = "minimal",
+          border = "rounded",
+          source = "always",
+          header = "",
+          prefix = "",
+        },
+      })
+      vim.keymap.set("n", "<leader>ws", function()
+        vim.lsp.buf.workspace_symbol()
+      end)
+      vim.keymap.set("n", "gr", function()
+        require("telescope.builtin").lsp_references()
+      end)
+      vim.keymap.set("n", "<leader>q", function()
+        vim.diagnostic.open_float()
+      end)
+      vim.keymap.set("n", "gd", function()
+        vim.lsp.buf.definition()
+      end)
+      vim.keymap.set("n", "<leader>l", function()
+        vim.diagnostic.goto_next()
+      end)
+      vim.keymap.set("n", "<leader>h", function()
+        vim.diagnostic.goto_prev()
+      end)
+    end,
+  },
 }
