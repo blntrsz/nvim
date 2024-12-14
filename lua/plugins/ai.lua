999269f9767920004708df6804a0ad9d4a9e5ea8
Balint Orosz <orosz.balint.97@gmail.com>
2024-12-14 20:38:22 +0100

    add supermaven on personal

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,3 +1,19 @@
-return {
-  "github/copilot.vim",
-}
+local stage = require("stage")
+
+if stage == "personal" then
+  return {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({})
+    end,
+  }
+end
+
+if stage == "work" then
+  return {
+    "github/copilot.vim",
+  }
+end
+
+
+return {}
