380d49ddbe41859735ed0a205d70fb954b9be82e
Balint Orosz <orosz.balint.97@gmail.com>
2024-12-16 09:17:50 +0100

    add quick fix close on select

--- init.lua
+++ init.lua
@@ -4,3 +4,7 @@ require("config.lazy")
 require("config.multigrep")
 
 vim.keymap.set("n", "<leader>/", require("config.multigrep").live_multi_grep)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "qf" },
+  command = [[nnoremap <buffer> <CR> <CR>:cclose<CR>]],
+})
