210dc2227a82643d10fc2b905e0beb4eef958e19
Balint Orosz <orosz.balint.97@gmail.com>
2024-12-24 19:52:32 +0100

    remove tab completion

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -110,9 +110,6 @@ return {
           ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
           ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
           ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<Tab>"] = cmp.mapping.select_next_item({ behaviour = cmp.SelectBehavior.Insert }),
-          ["<S-Tab>"] = cmp.mapping.select_prev_item({ behaviour = cmp.SelectBehavior.Insert }),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
       })
