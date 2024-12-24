9a1e7771deb7f0f15e26575d7a1e7f5b11650dc4
Balint Orosz <orosz.balint.97@gmail.com>
2024-12-24 19:30:03 +0100

    fix harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,8 +3,8 @@ return {
     "theprimeagen/harpoon",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
-      { "<leader>hh", "<cmd>lua require('harpoon.mark').add_file()<cr>",        desc = "Harpoon add mark" },
-      { "<leader>ha", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Harpoon quick menu" },
+      { "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>",        desc = "Harpoon add mark" },
+      { "<leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Harpoon quick menu" },
       { "<leader>1",  "<cmd>lua require('harpoon.ui').nav_file(1)<cr>",         desc = "Harpoon nav file 1" },
       { "<leader>2",  "<cmd>lua require('harpoon.ui').nav_file(2)<cr>",         desc = "Harpoon nav file 2" },
       { "<leader>3",  "<cmd>lua require('harpoon.ui').nav_file(3)<cr>",         desc = "Harpoon nav file 3" },
